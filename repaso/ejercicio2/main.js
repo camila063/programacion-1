8ca7a613e8cb6b96c355e213bdd267e8d15663e6
camila063 <vilarroelcamila49@gmail.com>
2022-04-19 16:56:16 -0300

    repaso

--- repaso/ejercicio2/main.js
+++ repaso/ejercicio2/main.js
@@ -7,7 +7,6 @@ const mostrar = () => {
     const dato1 = document.getElementById("dato1").value
     const dato2 = document.getElementById("dato2").value 
     const dato3 = document.getElementById("dato3").value 
-    //ejecutando la funcion
     const respuesta = calcular(dato1,dato2,dato3)
     document.getElementById("h_resultado").textContent = respuesta
 

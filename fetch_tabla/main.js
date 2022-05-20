e56814016b5173b6e8f0872fd323e3bd93947016
camila063 <vilarroelcamila49@gmail.com>
2022-05-20 17:07:15 -0300

    tablas

---
+++ fetch_tabla/main.js
@@ -0,0 +1,21 @@
+const  obtener_usuarios = async () =>{
+   const usuarios= await fetch('https://jsonplaceholder.typicode.com/users')
+   const usuarios_definidos = await usuarios.json()
+
+   let filas = []
+   //recorrer los usuarios
+   usuarios_definidos.forEach(element => {
+       let fila = 
+       `<tr>
+            <td><img src="" alt=""></td>
+            <td>${element.username}</td>
+            <td>${element.email}</td>
+            <td>${element.phone}</td>    
+       </tr>`
+       filas.push(fila)
+       
+   });
+   document.getElementById("tbl_body").innerHTML = filas.join('')
+}
+//invocamos a la función 
+obtener_usuarios()

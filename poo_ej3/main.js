3fc0ea79a7590f45cf4883f6020f656cd8ba5974
camila063 <vilarroelcamila49@gmail.com>
2022-06-30 17:02:25 -0300

    poo_ej3

--- poo_ej3/main.js
+++ poo_ej3/main.js
@@ -1,2 +1,17 @@
 import Producto from "./producto.js";
-const product = new Producto
+function guardar(){
+    let descripcion = document.getElementById("inp_descripcion").value
+    let precio_venta = document.getElementById("inp_precio").value
+    let categoria = document.getElementById("slt_cat").value
+    let producto = new Producto(descripcion,precio_venta,categoria)
+
+    //invocamos al metodo guardar_producto()
+    //perteneciente a la clase Producto
+    producto.guardar_producto()
+}
+document.getElementById("btn_guardar").addEventListener("click",guardar)
+function listar(){
+    let producto = new Producto
+    producto.obtener_productos()
+}
+listar()
--- poo_ej3/producto.js
+++ poo_ej3/producto.js
@@ -1,6 +1,32 @@
 export default class Producto{
-    constructor()
+    constructor(d,pv,c){
+        this.descripcion = d
+        this.precio_venta = pv
+        this.categoria = c
+    }
+    ///metodo
     guardar_producto(){
+        let nuevo_producto = {
+            descripcion:this.descripcion,
+            precio_venta:this.precio_venta,
+            categotia:this.categoria
+        }
+
+        if("productos" in localStorage){
+            /*convertimos el JSON ontenido desde el storage en una expresion nativa de javascript*/
+            let lista_productos = JSON.parse(localStorage.getItem("productos") )
+            lista_productos.push(nuevo_producto)
+            localStorage.setItem("productos",JSON.stringify(lista_productos) )
+        }else{
+            let lista_productos = []
+            lista_productos.push(nuevo_producto)
+    
+            //guardar en el storage
+            localStorage.setItem("productos",JSON.stringify(lista_productos) )
+
+        }
+    }
+    obtener_productos(){
         
     }
 }

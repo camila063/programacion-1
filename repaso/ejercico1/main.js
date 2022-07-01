258c13d0ae53ac4855fa78ce2c0a6a9c8257f6e8
camila063 <vilarroelcamila49@gmail.com>
2022-07-01 17:09:00 -0300

    A

---
+++ repaso/ejercico1/main.js
@@ -0,0 +1,8 @@
+function calcular(temperatura){
+    const resultado = temperatura *(1.8) + 32   
+    return resultado
+}
+function mostrar (){
+    const respuesta = calcular
+    document.getElementById(h_respuesta).textContent = respuesta
+}
